861954b065ac63c0a2a94987a4d0f09aa8771cfd
Omar <ofali@gmail.com>
2024-03-21 20:59:55 +0200

    Part 11 Department Controller - Create

--- Route.C41.G02.PL/Controllers/DepartmentController.cs
+++ Route.C41.G02.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Route.C41.G02.BLL.Interfaces;
 using Route.C41.G02.BLL.Repositories;
+using Route.C41.G02.DAL.Models;
 
 namespace Route.C41.G02.PL.Controllers
 {
@@ -16,6 +17,22 @@ namespace Route.C41.G02.PL.Controllers
         {
             var Departments = _DepartmentRepository.GetAll();
             return View(Departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var count = _DepartmentRepository.Add(department);
+                if(count > 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(department);
         }
     }
 }
