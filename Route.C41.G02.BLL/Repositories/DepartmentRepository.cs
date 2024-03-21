c37a1d0c0cd28669e556a0d3aeb8b58550bf11ab
Omar <ofali@gmail.com>
2024-03-21 16:56:41 +0200

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ Route.C41.G02.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G02.BLL.Interfaces;
+using Route.C41.G02.DAL.Data;
+using Route.C41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G02.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Department Department)
+        {
+            _dbContext.Departments.Add(Department);
+            return _dbContext.SaveChanges();
+        }
+        public int Delete(Department Department)
+        {
+            _dbContext.Departments.Remove(Department);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(Department Department)
+        {
+            _dbContext.Departments.Update(Department);
+            return _dbContext.SaveChanges();
+        }
+        public Department Get(int id)
+            => _dbContext.Departments.Find(id);
+        public IEnumerable<Department> GetAll()
+            => _dbContext.Departments.AsNoTracking().ToList();
+    }
+}
